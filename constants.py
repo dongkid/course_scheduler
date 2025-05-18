698c670f078a0454879ce1aae31afcdc9994cd2e
dongkid <rxie5012@gmail.com>
2025-05-18 11:30:53 +0800

    build: 更新程序版本号
    - 将版本号从 0.1.15-preview 修改为 0.1.15-preview1

--- constants.py
+++ constants.py
@@ -8,6 +8,6 @@ WEEKDAYS = ["一", "二", "三", "四", "五", "六", "日"]
 # 程序信息
 APP_NAME = "桌面课程表 - Course Scheduler"
 AUTHOR = "dong"
-VERSION = "0.1.15-preview"
+VERSION = "0.1.15-preview1"
 CONFIG_VERSION = "1"
 PROJECT_URL = "https://github.com/dongkid/course_scheduler"
